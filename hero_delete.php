48891202c5a8665a86db3b212b1d92509ae57fa5
SVValen <valensosa157@gmail.com>
2021-01-12 15:36:37 -0300

    fix

--- hero_delete.php
+++ hero_delete.php
@@ -5,24 +5,28 @@ header("Access-Control-Allow-Origin: *");
 include_once "config.php";
 include_once "sql_srv.php";
 
+$id = $_SERVER['QUERY_STRING']; 
+//lleva y trae datos
+//como es un GET , tiene que enviar el id que va a borrar en este caso
+
 $input = file_get_contents("php://input");
 $data = json_decode($input, true);
 
 $db = SQLSRV::connect();
 
 $stmt = sqlsrv_query($db,
-    "DELETE FROM Heroes SET name = ?
-    WHERE id = ?", ["id"] );
+    "DELETE FROM Heroes WHERE id = ?",
+    [$id] );
 
-if($stmt === false) {
-    SQLSRV::error(500, 'Error interno del servidor', $db);
+if ($stmt === false) {
+    SQLSRV::error(500, "Error interno del servidor", $db);
 }
 
 sqlsrv_fetch($stmt);
 
 sqlsrv_free_stmt($stmt);
+
 SQLSRV::close($db);
 
-echo $input;
-
+echo json_encode([]);
 ?>
--- hero_post.php
+++ hero_post.php
@@ -1,39 +1,39 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: *");
 
-include_once "config.php";
-include_once "sql_srv.php";
+    include_once "config.php";
+    include_once "sql_srv.php";
 
-$input = file_get_contents("php://input");
-$data = json_decode($input, true);
+    $input = file_get_contents("php://input");
+    $data = json_decode($input, true);
 
-$db = SQLSRV::connect();
+    $db = SQLSRV::connect();
 
-$stmt = sqlsrv_query($db,
-    "INSERT INTO Heroes VALUES (?);
-    SELECT @@IDENTITY id;",
-     [$data["name"]] );
+    $stmt = sqlsrv_query($db,
+        "INSERT INTO Heroes ( name ) VALUES (?);
+        SELECT @@IDENTITY id;",
+        [$data["name"]] 
+    );
 
-if($stmt === false) {
-    SQLSRV::error(500, 'Error interno del servidor', $db);
-}
+    if ($stmt === false){
+        SQLSRV::error(500, "Error interno del servidor", $db);
+    }
 
-$results = [];
+    $results= [];
 
-sqlsrv_fetch($stmt);
+    sqlsrv_fetch($stmt);
 
-sqlsrv_next_result($stmt);
+    sqlsrv_next_result($stmt);
 
-$row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
 
-$results = $data;
-$results["id"] = $row["id"];
+    $results = $data;
+    $results["id"] = $row["id"];
 
-sqlsrv_free_stmt($stmt);
+    sqlsrv_free_stmt($stmt);
 
-SQLSRV::close($db);
+    SQLSRV::close($db);
 
-echo $input;
-xml_error_string
+    echo json_encode($results);
 ?>
