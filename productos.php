b980835ac9a0b205b3586a07d059f2274e293d01
SVValen <valensosa157@gmail.com>
2021-01-15 17:06:27 -0300

    refactory productos

--- productos.php
+++ productos.php
@@ -1,107 +1,45 @@
 <?php
 include_once 'init.php';
+include_once './modules/productos.php';
 
-$results = [];
+$response = [];
 $db = SQL::connect();
+
+$model = new Producto();
 
 //-------------------------------------GET
 //-------------------------------------buscar / lista productos
 
 if (METHOD == 'GET' && !defined("ID")) {
-    $sql = "SELECT prodId,
-            prodDescripcion, 
-            prodPrecio,
-            CONVERT(VARCHAR, prodFechaAlta, 126) prodFechaAlta,
-            prodBorrado
-            FROM Producto
-            WHERE prodBorrado = 0";
-
-    $params=null;
-    
-    if (isset( $_GET["prodDescripcion"])){
-        $params = ["%" . $_GET["prodDescripcion"] . "%"];
-        $sql = $sql . " AND prodDescripcion LIKE ? ";
-    };
-
-    $stmt = SQL::query($db, $sql, $params);
-
-    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-        $results[] = $row;
-    }
+    $response  = $model->get($db);
 }
 
 //------------------------------------GET/ID
 //------------------------------------un solo producto
 
 if (METHOD == 'GET' && defined("ID")) {
-    $stmt = SQL::query ($db,
-            "SELECT prodId,
-            prodDescripcion, 
-            prodPrecio,
-            CONVERT(VARCHAR, prodFechaAlta, 126) prodFechaAlta,
-            prodBorrado
-            FROM Producto
-            WHERE prodId = ? ",
-            [ID]);
-
-    $results = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-}
+    $response = $model->getId ($db);
+    }
 
 //------------------------------------DELETE
 //------------------------------------borrar producto
 
 if (METHOD == 'DELETE' && defined("ID")) {
-    $stmt = SQL::query ($db,
-            "UPDATE Producto
-            SET prodBorrado = 1
-            WHERE prodId = ?",
-            [ID]);
-    
-    $results = [];
+    $response = $model->delete($db);
 }
-
 
 //-------------------------------------POST
 //-------------------------------------agregar producto
 
 if (METHOD == 'POST') {
-    $stmt = SQL::query($db,
-
-    "INSERT INTO Producto
-    ( prodDescripcion,
-    prodPrecio,
-    prodFechaAlta,
-    prodBorrado)
-    VALUES (?,?,GETDATE(),0);
-
-    SELECT @@IDENTITY prodId, CONVERT(VARCHAR, GETDATE(), 126) prodFechaAlta;",
-
-    [DATA["prodDescripcion"],DATA["prodPrecio"]]
-    );
-
-    sqlsrv_fetch($stmt);
-    sqlsrv_next_result($stmt);
-
-    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
-
-    $results[] = DATA;
-    $results['prodId'] = $row['prodId'];
-    $results['prodFechaAlta'] = $row['prodFechaAlta'];
-    $results['prodBorrado'] = 0;
+    $response = $model->post($db);
 }
 
 //----------------------------------PUT
 //----------------------------------actualizar producto
 
 if (METHOD == 'PUT') {
-    $stmt = SQL::query($db,
-    "UPDATE Producto
-    SET prodDescripcion = ?,
-        prodPrecio = ?
-    WHERE prodId = ?",
-    [DATA["prodDescripcion"], DATA["prodPrecio"], DATA["prodId"]]);
-
-    $results = [];
+    $response = $model->put($db);
 }
 
 if (isset($stmt)) {
@@ -110,7 +48,7 @@ if (isset($stmt)) {
 }
 
 
-echo json_encode($results)
+echo json_encode($response)
 
 
 ?>
